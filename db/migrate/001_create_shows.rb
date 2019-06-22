3cbeb42a0f06aa0bfcbcd73f413a0e1df1411db7
AJ Guiles <ashiguiles@gmail.com>
2019-06-22 19:15:25 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,13 @@
 class CreateShow < ActiveRecord::Migration
   
   def change 
-    
-    
-end
-end
+    create_table :shows do |t|
+      t.string = name 
+      t.string = network
+      t.string = day
+      t.integer = rating 
+   end
+  end
   
   
 end
