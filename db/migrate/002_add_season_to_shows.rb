3c323163f47802203794c1c9a01f22aac474cc3d
AJ Guiles <ashiguiles@gmail.com>
2019-06-22 20:19:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,17 +1,5 @@
-# class AddSeasonToShows < ActiveRecord::Migration[5.2]
-  
-#   def change 
-#     add_column 
-#     :shows, :season, :string 
-  
-#   end
-  
-  
-# end
-
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    add_column
-    :shows, :season, :string
+    add_column :shows, :season, :string
   end
 end
